26dd9a8a11c44e8a83f118774850065417108d63
Keith Ma <keithfma@gmail.com>
2023-12-02 13:14:13 -0500

    Cleanup day 1

--- day-01/calibrate.py
+++ day-01/calibrate.py
@@ -6,8 +6,6 @@ import re
 import click
 
 
-INPUT_PATH = Path('input.txt')
-
 WORD_TO_DIGIT = {
     'one': '1',
     'two': '2',
@@ -25,55 +23,27 @@ DIGITS = set(WORD_TO_DIGIT.values())
 WORDS = set(WORD_TO_DIGIT.keys())
 
 
-def ensure_digit(x: str) -> str:
-    try:
-        return WORD_TO_DIGIT[x]
-    except KeyError:
-        return x
+def part_i(txt: str):
 
-
-pattern = re.compile(r'\d|one|two|three|four|five|six|seven|eight|nine')
-
-
-
-def part_i(txt):
-        
     total = 0
-    
     for line in txt.splitlines():
         
-        first_digit = None
-        last_digit = None
-
-        for x in line:
-            if x in DIGITS:
-                if first_digit is None:
-                    first_digit = x
-                else:
-                    last_digit = x
+        digits = []
         
-        if not last_digit:
-            last_digit = first_digit
+        for idx in range(len(line)):
+            
+            # check for character digits
+            if line[idx] in DIGITS:
+                digits.append(line[idx])
 
-        this = int(first_digit + last_digit)
-        print(this)
+        this = int(digits[0] + digits[-1])
         total += this
-
+        print(f"{line=}, {digits=}, {this=}, {total=}")
+            
     print(f'{total=}')
 
 
 def part_ii(txt: str):
-
-    total = 0
-    for line in txt.splitlines():
-        matches = pattern.findall(line)
-        this = int(ensure_digit(matches[0]) + ensure_digit(matches[-1]))
-        total += this
-        print(f"{line=}, {matches=}, {this=}, {total=}")
-    print(f'{total=}')
-
-
-def part_iib(txt: str):
 
     total = 0
     for line in txt.splitlines():
@@ -99,7 +69,6 @@ def part_iib(txt: str):
     print(f'{total=}')
             
 
-
 if __name__ == '__main__':
 
     _, input_path, part = sys.argv
@@ -111,7 +80,7 @@ if __name__ == '__main__':
         part_i(input_txt)
 
     elif part == '2':
-        part_iib(input_txt)
+        part_ii(input_txt)
 
     else:
         raise ValueError('Invalid {part=}')
