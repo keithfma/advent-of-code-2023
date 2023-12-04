a8c7db5d2c916180c6d584316422ae21d1b4a31b
Keith Ma <keithfma@gmail.com>
2023-12-03 20:11:22 -0500

    Part 1 completed

--- day-03/schematic.py
+++ day-03/schematic.py
@@ -1,47 +1,80 @@
 """Advent of Code 2023 - Day 3"""
 
 import sys
+from attrs import frozen
 from pprint import pprint
-import numpy as np
 
 
-def parse_arrays(filename: str) -> tuple[np.ndarray, np.ndarray]:
-    """Return parsed input file as integer array of part numbers, and boolean array of symbol locations
-    """
+DIGITS = '0123456789'
+NULL = '.'
+
+
+@frozen
+class Loc:
+    row: int
+    col: int
+
+
+def part_i(filename: str):
+
     with open(filename, 'r') as fp:
+        txt = fp.read().strip()
         
-        numbers = []
-        symbols = []
-        for line in fp.readlines():
-            numbers_row = []
-            symbols_row = []
-            for elem in line.strip().replace('.', '0'):
-                try:
-                    # if the element is an integer, store that value and not that it is not a symbol
-                    numbers_row.append(int(elem))
-                    symbols_row.append(False)
-                except ValueError:   
-                    # if the element is a symbol, treat its value as 0 and note that it *is* a symbol
-                    numbers_row.append(0)
-                    symbols_row.append(True)
-            numbers.append(numbers_row)
-            symbols.append(symbols_row)
-    return np.array(numbers, dtype=int), np.array(symbols, dtype=bool)
+    # find locations adjacent to symbols
+    include: set[Loc] = set()
+    for ii, row in enumerate(txt.splitlines()):
+        for jj, elem in enumerate(row):
+            if elem not in NULL + DIGITS:
+                # a symbol! store this location and all neighbors. It does not matter if the
+                #   neighbor locations are in-bounds or not
+                for delta_i in [-1, 0, 1]:
+                    for delta_j in [-1, 0, 1]:
+                        include.add(Loc(ii + delta_i, jj + delta_j))
+            else:
+                # not a symbol, do nothing
+                pass
+    
+    num_rows = ii + 1
+    num_cols = jj + 1
+        
+    # find part numbers and sum if they are to be included
+    total = 0
+    for ii, row in enumerate(txt.splitlines()):
 
+        nums = []
+        locs = set()
+
+        for jj, elem in enumerate(row):
+            if elem in DIGITS:
+                # a digit in a number! append it
+                nums.append(elem)
+                locs.add(Loc(ii, jj))
+                
+            if nums and (elem not in DIGITS or jj == num_cols - 1):
+                # we have a part number in progress and have either reached its end or reached the end of the row!
+                # complete the part number, and sum if it should be included
+                value = int(''.join(nums)) 
+                if include.intersection(locs):
+                    total += value
+                else:
+                    pass
+
+                # reset for next part number
+                nums = []
+                locs = set()
+                
+               
+    print(f'{total=}')
 
 if __name__ == '__main__':
 
     _, input_path, part = sys.argv
     
     if part == '1':
-       pass 
+       part_i(input_path)
     else:
         raise ValueError(f'No such {part=}')
 
-    num, sym = parse_arrays(input_path)
 
-    mask = sym.copy()
-    mask[1:, :] = np.maximum(mask[1:, :], sym[:-1, :])  # down
-    mask[:-1, :] = np.maximum(mask[:-1, :], sym[1:, :])  # up
-    mask[:, :-1] = np.maximum(mask[:, :-1], sym[:, 1:])  # left
-    mask[:, 1:] = np.maximum(mask[:, 1:], sym[:, :-1])  # left
+
+            
