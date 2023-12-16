342081c532031d6651abdabcff787351df046a8e
Keith Ma <keithfma@users.noreply.github.com>
2023-12-16 10:22:30 -0500

    Day 04 (#3)

---
+++ day-04/winner.py
@@ -0,0 +1,75 @@
+import sys
+import attrs
+
+
+@attrs.define
+class Card:
+
+    id: int
+    win: set[int]
+    have: tuple[int]
+    copies: int = 1
+
+    @classmethod
+    def parse(cls, card_txt) -> 'Card':
+        id_txt, card_txt = card_txt.strip().split(':')
+        win_txt, have_txt = card_txt.split('|')
+        return cls(
+            id=int(id_txt.strip().split(' ')[-1]),
+            win={int(x) for x in win_txt.strip().split(' ') if x},
+            have=tuple(int(x) for x in have_txt.strip().split(' ') if x),
+        )
+
+    def num_matches(self) -> int:
+        return sum(1 if num in self.win else 0 for num in self.have)
+
+    def score(self) -> int:
+        power = self.num_matches() - 1
+        if power: 
+            return 2**power
+        return 0
+
+
+def part_i(filename: str):
+
+    with open(filename, 'r') as fp:
+        lines = fp.readlines()
+    
+    total_value = 0
+    for line in lines: 
+        card = Card.parse(line)
+        total_value += card.score()
+
+    print(f'{total_value=}')
+
+
+def part_ii(filename: str):
+
+    cards = [] 
+    with open(filename, 'r') as fp:
+        for line in fp.readlines():
+            cards.append(Card.parse(line))
+
+    # resolve cards and their copies
+    total_cards = 0
+    for ii, card in enumerate(cards):
+
+        total_cards += card.copies
+
+        for jj in range(ii + 1, ii + 1 + card.num_matches()):
+            cards[jj].copies += card.copies 
+
+    print(f'{total_cards=}')
+
+
+if __name__ == '__main__':
+
+    _, input_path, part = sys.argv
+    
+    if part == '1': 
+        part_i(input_path)
+    elif part =='2':
+        part_ii(input_path)
+    else:
+        raise ValueError(f'Bad part number: {part}')
+
