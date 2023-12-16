b7326c01352ce7e7c6d52eb37b40137364b01c9f
Keith Ma <keithfma@gmail.com>
2023-12-16 09:55:45 -0500

    Refactor to create a "Card" class for part 2

--- day-04/winner.py
+++ day-04/winner.py
@@ -1,4 +1,18 @@
 import sys
+import attrs
+
+
+@attrs.frozen
+class Card:
+
+    win: set[int]
+    have: tuple[int]
+
+    def score(self) -> int:
+        matches = sum(1 if num in self.win else 0 for num in self.have)
+        if matches: 
+            return 2**(matches - 1)
+        return 0
 
 
 def part_i(filename: str):
@@ -9,15 +23,11 @@ def part_i(filename: str):
     total_value = 0
     for line in lines: 
         win_txt, have_txt = line.strip().split(':')[1].split('|')
-        win = {int(x) for x in win_txt.strip().split(' ') if x}
-        have = tuple(int(x) for x in have_txt.strip().split(' ') if x)
-
-        value = 0
-        for num in have:
-            if num in win:
-                value = max(1, value*2)
-
-        total_value += value
+        card = Card(
+            win={int(x) for x in win_txt.strip().split(' ') if x},
+            have=tuple(int(x) for x in have_txt.strip().split(' ') if x),
+        )
+        total_value += card.score()
 
     print(f'{total_value=}')
 
