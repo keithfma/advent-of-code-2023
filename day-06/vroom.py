2412b1cb0dd269a422a542627b57c587c5a5754a
Keith Ma <keithfma@gmail.com>
2023-12-26 13:16:31 -0500

    Part 2 completed

--- day-06/vroom.py
+++ day-06/vroom.py
@@ -3,30 +3,6 @@ import attrs
 from math import sqrt, ceil, floor, prod
 
 
-@attrs.frozen
-class Race:
-
-    # total race time
-    t: int
-
-    # distance-to-beat
-    d: int
-
-
-
-def parse_races(filename: str) -> tuple[Race, ...]:
-
-    with open(filename, 'r') as fp:
-        
-        _, times_txt = fp.readline().split(':')
-        times = [int(x) for x in times_txt.strip().split(' ') if x]
-
-        _, distances_txt = fp.readline().split(':')
-        distances = [int(x) for x in distances_txt.strip().split(' ') if x]
-    
-    return tuple(Race(t, d) for t, d in zip(times, distances))
-
-
 def next_larger_int(num: float) -> int:
     if (larger := ceil(num)) > num:
         return larger
@@ -39,23 +15,56 @@ def next_smaller_int(num: float) -> int:
     return int(num - 1)
 
 
-def part_i(filename: str):
+@attrs.frozen
+class Race:
 
-    races = parse_races(filename)
-    
-    num_paths_to_victory = []
+    # total race time
+    t: int
 
-    for race in races:
-        # find two zeros of the quadratic equation 0 = charge_time**2 - charge_time*race_time + distance_to_beat 
-        # use the quadratic equation to solve for charge_time, with a=1, b=-race_time and c=distance_to_beat
-        # these zeros are the minimum and maximum charge time that will beat the target distance
-        left_term = race.t / 2
-        right_term = sqrt(race.t*race.t - 4*race.d) / 2
+    # distance-to-beat
+    d: int
+
+    def ways_to_win(self) -> int:
+        """Return number of ways to win this race"""
+        # solves quadratic equation for the charge time needed to match the distance-to-beat
+        #   then finds the min/max winning charge times asthe next higher/lower integers 
+        left_term = self.t / 2
+        right_term = sqrt(self.t*self.t - 4*self.d) / 2
         min_charge_time = next_larger_int(left_term - right_term)
         max_charge_time = next_smaller_int(left_term + right_term)
-        num_paths_to_victory.append(max_charge_time - min_charge_time + 1)
+        return max_charge_time - min_charge_time + 1
 
-    print(f'Ways to win: {prod(num_paths_to_victory)}')
+
+def part_i(filename: str):
+
+    with open(filename, 'r') as fp:
+        
+        _, times_txt = fp.readline().split(':')
+        times = [int(x) for x in times_txt.strip().split(' ') if x]
+
+        _, distances_txt = fp.readline().split(':')
+        distances = [int(x) for x in distances_txt.strip().split(' ') if x]
+    
+    races = tuple(Race(t, d) for t, d in zip(times, distances))
+
+    ways_to_win = [r.ways_to_win() for r in races]
+
+    print(f'Ways to win: {prod(ways_to_win)}')
+
+
+def part_ii(filename: str):
+
+    with open(filename, 'r') as fp:
+        
+        _, time_txt = fp.readline().split(':')
+        time = int(''.join(x for x in time_txt.strip().split(' ') if x))
+
+        _, distance_txt = fp.readline().split(':')
+        distance = int(''.join(x for x in distance_txt.strip().split(' ') if x))
+    
+    race = Race(time, distance)
+
+    print(f'Ways to win: {race.ways_to_win()}')
 
 
 if __name__ == '__main__':
@@ -64,6 +73,9 @@ if __name__ == '__main__':
     
     if part_number == '1': 
         part_i(input_file) 
+
+    elif part_number == '2': 
+        part_ii(input_file) 
 
     else:
         raise NotImplementedError(f'Unknown part number: {part_number}')
