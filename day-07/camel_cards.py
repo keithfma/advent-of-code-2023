0af157120ba842ca1130cd90ef7d016970080c7e
Keith Ma <keithfma@gmail.com>
2023-12-27 15:54:13 -0500

    Refactor to prepare for part 2

--- day-07/camel_cards.py
+++ day-07/camel_cards.py
@@ -2,18 +2,30 @@ import sys
 from enum import Enum
 import attrs
 from collections import Counter
-from operator import attrgetter
+from operator import attrgetter, methodcaller
 
 
 @attrs.frozen
 class Hand:
     
-    cards: tuple[int, int, int, int, int]
-    bid: int
-    type: int = attrs.field(init=False)
+    cards: str
 
-    @type.default
-    def _set_type(self) -> int:
+    bid: int
+
+    def card_values(self, jokers: bool = False) -> tuple[int, int, int, int, int]:
+        specials = {'A': 14, 'K': 13, 'Q': 12, 'J': 1 if jokers else 11, 'T': 10}
+        values = []
+        for card in self.cards:
+            try:
+                values.append(specials[card])
+            except KeyError:
+                values.append(int(card))
+        return tuple(values)
+
+    def hand_type(self, jokers: bool = False) -> int:
+
+        if jokers:
+            raise NotImplementedError
 
         counts = tuple(sorted(Counter(self.cards).values(), reverse=True))
 
@@ -32,45 +44,38 @@ class Hand:
         if counts == (1, 1, 1, 1, 1):
             return 1  # high card
         raise ValueError(f'Unknown hand type: {self.cards}')
-    
 
 
 def parse_hands(filename: str) -> list[Hand]:
         
     hands = []
 
-    specials = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
-
     with open(filename, 'r') as fp:
         
         for line in fp.readlines():
-            cards_txt, bid_txt = line.strip().split()
-            
-            cards = []
-            for card_txt in cards_txt.strip():
-                try:
-                    cards.append(int(card_txt))
-                except ValueError:
-                    cards.append(specials[card_txt])
-            
-            hands.append(Hand(tuple(cards), int(bid_txt)))
+            cards, bid_txt = line.strip().split()
+            hands.append(Hand(cards, int(bid_txt)))
     
     return hands
 
 
-def sort_hands(values: list[Hand]) -> None:
-    """worst to best"""
-    values.sort(key=attrgetter('cards'))
-    values.sort(key=attrgetter('type'))
+def sort_hands(values: list[Hand], jokers: bool = False) -> list[Hand]:
+    """worst to best
+    See https://docs.python.org/3/howto/sorting.html#sort-stability-and-complex-sorts
+    """
+    return sorted(
+        sorted(values, key=methodcaller('card_values', jokers)),
+        key=methodcaller('hand_type', jokers)
+    )
 
 
-def part_i(filename: str):
+def get_winnings(filename: str, jokers: bool):
 
     hands = parse_hands(filename)
-    sort_hands(hands)  # in-place sort
+    sorted_hands = sort_hands(hands, jokers) 
     
     winnings = 0
-    for rank, hand in enumerate(hands, start=1):
+    for rank, hand in enumerate(sorted_hands, start=1):
         winnings += rank * hand.bid
 
     print(f'{winnings=}')
@@ -86,13 +91,13 @@ if __name__ == '__main__':
     input_filename, part_number = sys.argv[1:]
 
     if part_number == '1':
-        part_i(input_filename)
+        get_winnings(input_filename, jokers=False)
 
-    # elif part_number == '2':
-    #     part_ii(input_filename)
+    elif part_number == '2':
+        get_winnings(input_filename, jokers=True)
 
-    # else:
-    #     raise ValueError(f'Invalid part number: {part_number}')
+    else:
+        raise ValueError(f'Invalid part number: {part_number}')
 
 
         
