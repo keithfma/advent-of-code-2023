6cda4e269752d399d3085fa6408056eaacc18055
Keith Ma <keithfma@gmail.com>
2023-12-27 16:21:24 -0500

    Tidy up code

--- day-07/camel_cards.py
+++ day-07/camel_cards.py
@@ -7,12 +7,18 @@ from operator import attrgetter, methodcaller
 
 @attrs.frozen
 class Hand:
+    """A five-card hand for the Camel Cards game"""
     
+    # cards as a 5-character string
     cards: str
 
+    # bid amount
     bid: int
 
     def card_values(self, jokers: bool = False) -> tuple[int, int, int, int, int]:
+        """Card values as (sortable) integers. Better cards have higher values.
+        Optionally, treat "J" cards as jokers
+        """
         specials = {'A': 14, 'K': 13, 'Q': 12, 'J': 1 if jokers else 11, 'T': 10}
         values = []
         for card in self.cards:
@@ -23,22 +29,27 @@ class Hand:
         return tuple(values)
 
     def hand_type(self, jokers: bool = False) -> int:
+        """Hand type as a (sortable) integer. Better hands have higher hand types.
+        Optionally, treat "J" cards as jokers
+        """
 
+        # count cards, treating J as a wildcard if requested
         counter = Counter(self.cards)
 
         if jokers:
-            # hand is improved most by adding more of the most common card
             num_jokers = counter.pop('J', 0)
-            counts = sorted(counter.values(), reverse=True)
-            if counts:
-                counts[0] += num_jokers
-            else:
+            if num_jokers == 5:
+                # all jokers, that is 5-of-a-kind
                 counts = [num_jokers]
+            else:
+                # hand is improved most by adding more of the most common card
+                counts = sorted(counter.values(), reverse=True)
+                counts[0] += num_jokers
 
         else:
             counts = sorted(counter.values(), reverse=True)
 
-
+        # classify hand based on card count
         if counts == [5,]:
             return 7  # 5 of a kind
         if counts == [4, 1]:
@@ -57,6 +68,7 @@ class Hand:
 
 
 def parse_hands(filename: str) -> list[Hand]:
+    """Parse input file as a list of Hands"""
         
     hands = []
 
@@ -70,8 +82,8 @@ def parse_hands(filename: str) -> list[Hand]:
 
 
 def sort_hands(values: list[Hand], jokers: bool = False) -> list[Hand]:
-    """worst to best
-    See https://docs.python.org/3/howto/sorting.html#sort-stability-and-complex-sorts
+    """Sort hands from worst-to-best by hand type (primary), then by card values (secondary)
+    See: https://docs.python.org/3/howto/sorting.html#sort-stability-and-complex-sorts
     """
     return sorted(
         sorted(values, key=methodcaller('card_values', jokers)),
@@ -80,6 +92,9 @@ def sort_hands(values: list[Hand], jokers: bool = False) -> list[Hand]:
 
 
 def get_winnings(filename: str, jokers: bool):
+    """Print total winnings for the hands in the input file
+    Optionally, treat "J" cards as jokers
+    """
 
     hands = parse_hands(filename)
     sorted_hands = sort_hands(hands, jokers) 
@@ -90,11 +105,6 @@ def get_winnings(filename: str, jokers: bool):
 
     print(f'{winnings=}')
         
-
-
-def part_ii(filename: str):
-    raise NotImplementedError
-
 
 if __name__ == '__main__':
 
